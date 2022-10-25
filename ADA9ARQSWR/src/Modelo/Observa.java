31930fc8ae9964c5fc9de10fd8ec7822ae979a41
sajeme <56236562+sajeme@users.noreply.github.com>
2022-10-25 02:35:16 -0500

    Add files via upload

---
+++ ADA9ARQSWR/src/Modelo/Observa.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Personal
+ */
+public interface Observa {
+         public void suscribirObservador(Observador observador);
+	public void desuscribirObservador(Observador observador);
+	public void notificarObservadores(String mensaje, Object parametro);
+}
